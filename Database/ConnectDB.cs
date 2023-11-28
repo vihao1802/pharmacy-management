0539139365dda53b984f2bee72e3c3a42b98b9c3
Mthinh <minhthinh9229@gmail.com>
2023-11-28 13:51:52 +0700

    28/11/2023

--- Database/ConnectDB.cs
+++ Database/ConnectDB.cs
@@ -14,13 +14,13 @@ namespace pharmacy_management.Database
         //string strCnn = "Data Source=LAPTOP-ULQT60JG; Database=ql_nhathuoc;Max Pool Size=1000;Integrated Security = True";
 
         // @author Thinh
-        //string strCnn = "Data Source=LAPTOP-LOJNVCRF\\SQLEXPRESS; Database=ql_nhathuoc; Max Pool Size=1000;Integrated Security = True";
+        string strCnn = "Data Source=LAPTOP-LOJNVCRF\\SQLEXPRESS; Database=ql_nhathuoc; Max Pool Size=1000;Integrated Security = True";
 
         // @author Duc
         //string strCnn = "Data Source=MSI\\LAMDUC;Initial Catalog=ql_nhathuoc;Integrated Security=True";
 
         //@author an
-        string strCnn = "Data Source=DESKTOP-F74SIEE;Initial Catalog=ql_nhathuoc;Max Pool Size=1000;Integrated Security=True";
+        //string strCnn = "Data Source=DESKTOP-F74SIEE;Initial Catalog=ql_nhathuoc;Max Pool Size=1000;Integrated Security=True";
 
         SqlConnection sqlConn; //Doi tuong ket noi CSDL
         SqlDataAdapter da;//Bo dieu phoi du lieu
